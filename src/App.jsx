b3da370fcdecb7e08b0c8a7b1070ae46265e85a0
moisesb12 <mb1cm13@gmail.com>
2020-09-12 10:50:43 -0400

    F1-P1-2: Se adaptó extr. de archivos en /src: Deleted: App.css, index.css, logo.svg. Changed: App.js x App.jsx. Added: /styles y /styles/tailwind.css. Add-Code: App.jsx, tailwind.css e index.js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    // *** Inicio código de prueba ***
+    <div>
+      <h1 className="text-red-900">Primera línea de prueba con (h1)</h1>
+      <h2>Segunda línea de prueba con (h2)</h2>
+      <h3 className="textNew">Tercera línea de prueba con (h3)</h3>
+    </div>
+    // *** Final código de prueba ***
+  );
+};
+
+export default App;
